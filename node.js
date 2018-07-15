91e37b466ec03f774e63859694c93cfb1e244d1c
zq <512641057@qq.com>
2018-07-15 08:32:57 +0800

    上传简历

---
+++ node.js
@@ -0,0 +1,13 @@
+var fs = require('fs')
+
+var dirName = process.argv[2] /
+fs.mkdirSync("./" + dirName) 
+process.chdir("./" + dirName) 
+fs.mkdirSync('css') 
+fs.mkdirSync('js') 
+
+fs.writeFileSync("./index.html", "")
+fs.writeFileSync("css/style.css", "")
+fs.writeFileSync("./js/main.js", "")
+
+process.exit(0)
